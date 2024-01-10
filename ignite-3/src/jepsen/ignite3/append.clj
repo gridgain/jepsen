646383279314ddc538ef63fdad5bf4c9d89a0cdd
Andrey Khitrin <akhitrin@gridgain.com>
2024-01-10 15:56:01 +0500

    Use Accessor protocol instead of hardcoded implementation

--- ignite-3/src/jepsen/ignite3/append.clj
+++ ignite-3/src/jepsen/ignite3/append.clj
@@ -66,45 +66,21 @@
         (with-open [write-rs (run-sql session txn sql-insert [k (str v)])]))
       [opcode k v])))
 
-(defn read! [ignite txn [_ k _]]
-  "Read value to the table by key, as list of numbers."
-  (let [r (with-open [session   (.createSession (.sql ignite))
-                      rs        (run-sql session txn sql-select [k])]
-            (let [s (if (.hasNext rs) (.stringValue (.next rs) 1) "")]
-              (->> (clojure.string/split s #",")
-                   (remove #(.isEmpty %))
-                   (map #(Integer/parseInt %))
-                   (into []))))]
-    [:r k r]))
-
-(defn append! [ignite txn [opcode k v]]
-  "Append value to the table by key."
-  (with-open [session   (.createSession (.sql ignite))
-              read-rs   (run-sql session txn sql-select [k])]
-    (if (.hasNext read-rs)
-      ; update existing list
-      (let [old-list    (.stringValue (.next read-rs) 1)
-            new-list    (str old-list "," v)]
-        (with-open [write-rs (run-sql session txn sql-update [new-list k])]))
-      ; create a new list
-      (with-open [write-rs (run-sql session txn sql-insert [k (str v)])]))
-    [opcode k v]))
-
-(defn invoke-ops [^Ignite ignite ops]
+(defn invoke-ops [^Ignite ignite acc ops]
   "Perform operations in a transaction."
   (let [txn (.begin (.transactions ignite))
         result (mapv #(case (first %)
-                        :r       (read! ignite txn %)
-                        :append  (append! ignite txn %))
+                        :r       (read! acc ignite txn %)
+                        :append  (append! acc ignite txn %))
                      ops)]
     (.commit txn)
     result))
 
-(defn invoke-with-retries [^Ignite ignite ops]
+(defn invoke-with-retries [^Ignite ignite acc ops]
   "Perform operations with repeats on IgniteException, each time in a new transaction."
   (loop [attempt 1]
     (if-let [r (try
-                 (invoke-ops ignite ops)
+                 (invoke-ops ignite acc ops)
                  (catch TransactionException te
                    (log/info "TransactionException:" (.getMessage te)))
                  (catch IgniteException ie
@@ -128,7 +104,7 @@
       (let [row (.next rs)]
         (log/info (.intValue row 0) ":" (.stringValue row 1))))))
 
-(defrecord Client [^Ignite ignite]
+(defrecord Client [^Ignite ignite acc]
   client/Client
   ;
   (open! [this test node]
@@ -143,8 +119,8 @@
   ;
   (invoke! [this test op]
     (let [ops   (:value op)
-          ; result (invoke-ops ignite ops)
-          result (invoke-with-retries ignite ops)
+          ; result (invoke-ops ignite acc ops)
+          result (invoke-with-retries ignite acc ops)
           overall-result (assoc op
                                 :type :info
                                 :value (into [] result))]
@@ -157,7 +133,7 @@
 
 (comment "for repl"
 
-(def c (client/open! (Client. nil) {} "127.0.0.1"))
+(def c (client/open! (Client. nil (SqlAccessor.)) {} "127.0.0.1"))
 (client/setup! c {})
 
 (client/invoke! c {} {:type :invoke, :process 0, :f :txn, :value [[:r 5 nil] [:r 6 nil]]})
@@ -177,7 +153,7 @@
     (merge
       (let [test-ops {:consistency-models [:strict-serializable]}]
         {:name      "append-test"
-         :client    (Client. nil)
+         :client    (Client. nil (SqlAccessor.))
          :checker   (app/checker test-ops)
          :generator (ignite3/wrap-generator (app/gen test-ops) (:time-limit opts))})
       opts)))
