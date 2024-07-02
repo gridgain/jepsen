bf97b6d7e76f6e9cba3cfc83525e82731543c7a7
Andrey Khitrin <akhitrin@gridgain.com>
2024-07-02 19:34:57 +0500

    Extract join-path and join-comma fns to make code a bit simpler

--- ignite-3/src/jepsen/ignite3.clj
+++ ignite-3/src/jepsen/ignite3.clj
@@ -9,19 +9,22 @@
             [jepsen.os.centos :as centos]
             [jepsen.os.debian :as debian]))
 
+(def join-path (partial clojure.string/join "/"))
+(def join-comma (partial clojure.string/join ","))
+
 (def server-dir "/opt/ignite3")
 
 (defn db-dir
   "Creates path to the DB main directory, or its subpath (items in 'more')."
   [test & more]
-  (clojure.string/join "/" (concat [server-dir (str (:flavour test) "-db-" (:version test))]
-                                   more)))
+  (join-path (concat [server-dir (str (:flavour test) "-db-" (:version test))]
+                     more)))
 
 (defn cli-dir
   "Creates path to the CLI main directory, or its subpath (items in 'more')."
   [test & more]
-  (clojure.string/join "/" (concat [server-dir (str (:flavour test) "-cli-" (:version test))]
-                                   more)))
+  (join-path (concat [server-dir (str (:flavour test) "-cli-" (:version test))]
+                     more)))
 
 (defn list-nodes
   "Creates a list of nodes the current node should connect to."
@@ -42,7 +45,7 @@
   (let [all-nodes (:nodes test)]
     (c/exec :sed :-i (str "s/defaultNode/" (node-name all-nodes node) "/")
                      (db-dir test "etc" "vars.env"))
-    (c/exec :sed :-i (str "s/\"localhost:3344\"/" (clojure.string/join ", " (list-nodes all-nodes node)) "/")
+    (c/exec :sed :-i (str "s/\"localhost:3344\"/" (join-comma (list-nodes all-nodes node)) "/")
                      (db-dir test "etc" "ignite-config.conf"))))
 
 (defn upload-wrapper!
@@ -61,12 +64,11 @@
   "Create a list of params to be passed into 'ignite cluster init' CLI command."
   (let [nodes       (:nodes test)
         name-fn     (partial node-name nodes)
-        comma-join  (partial clojure.string/join ",")
         ; use 1 storage node for cluster of 1-2 nodes, and 3 storage nodes for larger clusters
         cmg-size    (if (< 2 (count nodes)) 3 1)
         cmg-nodes   (take cmg-size nodes)]
     ["--name=ignite-cluster"
-     (str "--metastorage-group=" (comma-join (map name-fn cmg-nodes)))]))
+     (str "--metastorage-group=" (join-comma (map name-fn cmg-nodes)))]))
 
 (defn start!
   "Starts server for the given node."
