aae2c8eb5150f8e5eb1bbc95965256aba0f02a19
Andrei Khitrin <akhitrin@gridgain.com>
2025-05-05 18:16:44 +0500

    QA-6154 Adapt to changes in IGNITE-24035 (default MG and CMG)
    Simplify cluster initialization command. After IGNITE-24035, manual MG
    assignment is optional.

--- ignite-3/test/jepsen/ignite_test.clj
+++ ignite-3/test/jepsen/ignite_test.clj
@@ -13,27 +13,15 @@
 
 (deftest init-command-test
   (testing "Generation of init command sequence"
-    (let [test1 {:flavour "ignite3" :nodes ["n1"]}
-          test2 {:flavour "ignite3" :nodes ["n1" "n2"]}
-          test3 {:flavour "ignite3" :nodes ["n1" "n2" "n3"]}
-          test4 {:flavour "ignite3" :nodes ["n1" "n2" "n3" "n4"]}
-          test5 {:flavour "ignite3" :nodes ["n1" "n2" "n3" "n4" "n5"]}]
-      (is (= ["bin/ignite3" "cluster" "init" "--name=ignite-cluster" "--metastorage-group=node-1"]
-             (init-command test1)))
-      (is (= ["bin/ignite3" "cluster" "init" "--name=ignite-cluster" "--metastorage-group=node-1"]
-             (init-command test2)))
-      (is (= ["bin/ignite3" "cluster" "init" "--name=ignite-cluster" "--metastorage-group=node-1,node-2,node-3"]
-             (init-command test3)))
-      (is (= ["bin/ignite3" "cluster" "init" "--name=ignite-cluster" "--metastorage-group=node-1,node-2,node-3"]
-             (init-command test4)))
-      (is (= ["bin/ignite3" "cluster" "init" "--name=ignite-cluster" "--metastorage-group=node-1,node-2,node-3"]
-             (init-command test5)))))
+    (let [test1 {:flavour "ignite3" :nodes ["n1" "n2" "n3" "n4" "n5"]}]
+      (is (= ["bin/ignite3" "cluster" "init" "--name=ignite-cluster"]
+             (init-command test1)))))
 
   (testing "Use of extra init options"
     (let [test1 {:flavour               "ignite3"
                  :nodes                 ["n1" "n2" "n3"]
                  :extra-init-options    "--config-files=my.conf"}]
-      (is (= ["bin/ignite3" "cluster" "init" "--config-files=my.conf" "--name=ignite-cluster" "--metastorage-group=node-1,node-2,node-3"]
+      (is (= ["bin/ignite3" "cluster" "init" "--config-files=my.conf" "--name=ignite-cluster"]
              (init-command test1)))))
 
   (testing "Pass custom environment"
@@ -43,7 +31,7 @@
           test2 {:flavour       "ignite3"
                  :nodes         ["n1"]
                  :environment   "JAVA_HOME=/opt/java/jdk-open-17"}]
-      (is (= [:env "JAVA_HOME=/opt/java/jdk-open-11" "bin/gridgain9" "cluster" "init" "--name=ignite-cluster" "--metastorage-group=node-1"]
+      (is (= [:env "JAVA_HOME=/opt/java/jdk-open-11" "bin/gridgain9" "cluster" "init" "--name=ignite-cluster"]
              (init-command test1)))
-      (is (= [:env "JAVA_HOME=/opt/java/jdk-open-17" "bin/ignite3" "cluster" "init" "--name=ignite-cluster" "--metastorage-group=node-1"]
+      (is (= [:env "JAVA_HOME=/opt/java/jdk-open-17" "bin/ignite3" "cluster" "init" "--name=ignite-cluster"]
              (init-command test2))))))
